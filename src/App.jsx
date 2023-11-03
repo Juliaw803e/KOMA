4b85d60c1a3d1c7c4579c83ebbaf786d8d42908f
Juliaw803e <41002169+Juliaw803e@users.noreply.github.com>
2023-11-03 12:24:01 +0100

    Eventuellt routing kommentarer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+//import {useParams, BrowserRouter, Link, Outlet, Routes, Route} from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,5 +31,31 @@ function App() {
     </>
   )
 }
+
+/* Eventuell routing i kommentarer: 
+function ingredients(){
+  return(
+    <h1>välj ingredients </h1> 
+  )
+}
+
+function slutsida(){
+  return(
+    <h1>välj ingredients </h1> 
+  )
+}
+
+function PageRoute(){
+  return(
+  <BrowserRouter>
+    <Routes>
+         <Route path="/" element={<App />}/>
+         <Route path="/country/:cca3" element={<ingredients />}/>
+         <Route path="/sista" element={slutsida/>/>
+      </Routes>
+  </BrowserRouter>
+
+  )
+}*/
 
 export default App
