4da0f13c1127992692edc47d1994ad378d8891a8
Juliaw803e <41002169+Juliaw803e@users.noreply.github.com>
2023-11-22 17:18:26 +0100

    Revert "Revert ":))""
    This reverts commit ad48720893b28331a76fe2c46d80f487d55b5313.

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,5 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import React from 'react';
+import './App.css';
 import { useNavigate } from "react-router-dom"; 
 
 
@@ -27,11 +28,11 @@ const Result = () => {
 {message === 'Mums, din kaka blev perfekt!' ? (
         <div>
           <h5> rättsvarknapp som ska skcika till hem och göra ny kaka</h5>
-          <button onClick={goToIngredientsPage}>Baka en ny kaka</button>
+          <button className="bakaEnTill" onClick={goToIngredientsPage}>Baka en ny kaka</button>
         </div>
       ) : (
         <div>
-          <button onClick={goToIngredientsPage}>Baka igen</button>
+          <button className="bakaIgen" onClick={goToIngredientsPage}>Försök igen</button>
         </div>
       )}
 
