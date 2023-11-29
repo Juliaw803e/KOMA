407b41b58c69e84dfb02d4f315c7592aa9346aef
elsatarnbrant <elsa.tarnbrant@telia.com>
2023-11-29 15:11:47 +0100

    Revert "Merge branch 'main' of https://github.com/Juliaw803e/KOMA"
    This reverts commit a433dcb0acac075fae9a13f50acc3349d110c328, reversing
    changes made to db22df1cdac3d4de440dae53248c33c0a8c011f2.

--- src/Ingredients.jsx
+++ src/Ingredients.jsx
@@ -3,11 +3,11 @@ import { useNavigate } from "react-router-dom"; // Import useNavigate
 import './App.css';
 
 const Ingredients = ({
-  ingredientResponses,
-  setIngredientResponses,
-}) => {
-  const [currentIngredient, setCurrentIngredient] = useState(0);
-  const navigate = useNavigate(); // Initialize useNavigate
+    ingredientResponses,
+    setIngredientResponses,
+  }) => {
+    const [currentIngredient, setCurrentIngredient] = useState(0);
+    const navigate = useNavigate(); // Initialize useNavigate
 
 const barStyle = {
   width: parseInt(currentIngredient, 10) * 16 + '%', // Example width
@@ -87,93 +87,23 @@ const checkResults = () => {
     incorrectQuestions,
   };
 };
+  
+const ingredientsArray = ["mjöl", "socker", "smör", "ägg", "chokladknappar", "tid i ugnen"];
 
-  const question = [
-    {
-      text: "Välj hur mycket mjöl",
-      options: [
-        { id: 0, image: 'imageOne.jpg', isCorrect: false },
-        { id: 1, image: 'imageOne.jpg', isCorrect: true },
-        { id: 2, image: 'imageOne.jpg', isCorrect: false },
-      ]
-    },
-    {
-      text: "Välj hur mycket socker",
-      options: [
-        { id: 3, image: 'imageOne.jpg', isCorrect: true },
-        { id: 4, image: 'imageOne.jpg', isCorrect: false },
-        { id: 5, image: 'imageOne.jpg', isCorrect: false },
-      ]
-    },
-    {
-      text: "Välj hur mycket smör",
-      options: [
-        { id: 6, image: 'imageOne.jpg', isCorrect: false },
-        { id: 7, image: 'imageOne.jpg', isCorrect: true },
-        { id: 8, image: 'imageOne.jpg', isCorrect: false },
-      ]
-    },
-    {
-      text: "Välj hur många ägg",
-      options: [
-        { id: 9, image: 'imageOne.jpg', isCorrect: true },
-        { id: 10, image: 'imageOne.jpg', isCorrect: false },
-        { id: 11, image: 'imageOne.jpg', isCorrect: false },
-      ]
-    },
-    {
-      text: "Välj hur mycket chokladknappar",
-      options: [
-        { id: 12, image: 'imageOne.jpg', isCorrect: false },
-        { id: 13, image: 'imageOne.jpg', isCorrect: false },
-        { id: 14, image: 'imageOne.jpg', isCorrect: true },
-      ]
-    },
-    {
-      text: "Välj hur lång tid i ugnen",
-      options: [
-        { id: 15, image: 'imageOne.jpg', isCorrect: true },
-        { id: 16, image: 'imageOne.jpg', isCorrect: false },
-        { id: 17, image: 'imageOne.jpg', isCorrect: false },
-      ]
-    },
-  ]
+const handleNext = () => {
+  if (currentIngredient < question.length - 1) {
+    setCurrentIngredient(currentIngredient + 1);
+  } else {
+    const { allCorrect, incorrectQuestions } = checkResults();
 
-  const checkResults = () => {
-    // Check if all selected options are correct
-    const incorrectQuestions = ingredientResponses.reduce((incorrectIndices, response, index) => {
-      const correctOption = question[index].options.find(option => option.isCorrect);
+    const incorrectIngredientsList = incorrectQuestions.map(index => ingredientsArray[index]);
+    const resultMessage = allCorrect
+      ? "Mums, din kaka blev perfekt!" //if allCorrect == true 
+      : `Blä! Testa att ändra ${incorrectIngredientsList.join(', ')}`; //if allCorrect == false 
 
-      // Ensure that correctOption is defined and compare the response directly with correctOption.id
-      if (correctOption && correctOption.id !== response) {
-        incorrectIndices.push(index);
-      }
-
-      return incorrectIndices;
-    }, []);
-
-    return {
-      allCorrect: incorrectQuestions.length === 0,
-      incorrectQuestions,
-    };
-  };
-
-  const ingredientsArray = ["mjöl", "socker", "smör", "ägg", "chokladknappar", "tid i ugnen"];
-
-  const handleNext = () => {
-    if (currentIngredient < question.length - 1) {
-      setCurrentIngredient(currentIngredient + 1);
-    } else {
-      const { allCorrect, incorrectQuestions } = checkResults();
-
-      const incorrectIngredientsList = incorrectQuestions.map(index => ingredientsArray[index]);
-      const resultMessage = allCorrect
-        ? "Mums, din kaka blev perfekt!" //if allCorrect == true 
-        : `Blä! Testa att ändra ${incorrectIngredientsList.join(', ')}`; //if allCorrect == false 
-
-      navigate('/result', { state: { message: resultMessage } }); //skicka message i state. Message är resultmessage som kan ha två olika fall beroende på om allcorrect == true/false
-    }
-  };
+    navigate('/result', { state: { message: resultMessage } }); //skicka message i state. Message är resultmessage som kan ha två olika fall beroende på om allcorrect == true/false
+  }
+};
 
 
 return (
@@ -186,26 +116,30 @@ return (
           </div>
         </div>
       </div>
-      <div className="question-text">{<h2>{question[currentIngredient].text}</h2> /* renderar texten med vilken ingrediens */}</div>
-      <div className="radio-options-container"> 
-        {question[currentIngredient].options.map((option) => (
-          <div key={option.id} className="radio-option">
-            <label>
-              <input
-                type="radio"
-                value={option.id}
-                checked={ingredientResponses[currentIngredient] === option.id}
-                onChange={(e) => {
-                  const newResponses = [...ingredientResponses];
-                  newResponses[currentIngredient] = parseInt(e.target.value);
-                  setIngredientResponses(newResponses);
+
+    <img src="../images/kaka.png"></img>
+
+    <div className="question-text">{<h2>{question[currentIngredient].text}</h2> /* renderar texten med vilken ingrediens */}</div>
+    <ul>
+      {question[currentIngredient].options.map((option) => ( // map function som renderar en lista med alternativ. key hjälper att rendera 
+        <li key={option.id}>
+          <label>
+            <input
+              type="radio" //radio button 
+              value={option.id} //0,1,2 
+              checked={ingredientResponses[currentIngredient] === option.id} //kollar om currentingredient i ingredientresponses array = id. Om de matchar så "check:as" (väljs) knappen
+              onChange={(e) => { //onchange uppdaterar state med vad man valt. 
+                const newResponses = [...ingredientResponses]; //en kopia av ingredienrespons array skapas 
+                newResponses[currentIngredient] = parseInt(e.target.value);
+                setIngredientResponses(newResponses); //state uppdateras med setIngredientResponse 
                 }}
               />
               <img src={option.image} alt={`Option ${option.id}`} />
             </label>
-          </div>
+          </li>
         ))}
-      </div>
+      </ul>
+
       {currentIngredient !== 0 && (
        <button className = "navigation-button-left" onClick={() => setCurrentIngredient(currentIngredient - 1)}>
          Back<img src="../images/pilv.png"></img>
